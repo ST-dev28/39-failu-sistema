f9e1a013996e29b9f689e97c6d4f9f9c6f191be5
SandraT <sndrbltr@gmail.com>
2021-08-08 16:57:35 +0000

    Notes added

--- files.js
+++ files.js
@@ -1,3 +1,5 @@
+// consoleje irasyti ->>  node files.js
+
 const _data = require('./lib/async-data');
 const helpers = require('./lib/helpers');
 
@@ -8,15 +10,15 @@ const helpers = require('./lib/helpers');
     const petrasObj = helpers.parseJsonToObject(petras);
     console.log(petrasObj);
 
-    const maryjosObj = { ...petrasObj, name: 'Maryja' };
+    const maryjosObj = { ...petrasObj, name: 'Maryja' };   // sukuriam faila "maryja.json" pagal "pertas.json" duomenis pakeiciant varda
     const maryja = await _data.create('users', 'maryja', maryjosObj);
     console.log('Ar pavyko sukurti Maryja:', maryja);
 
-    const updatedMaryjosObj = { ...maryjosObj, age: 50 };
+    const updatedMaryjosObj = { ...maryjosObj, age: 50 };     // keiciam amziu
     const updatedMaryja = await _data.update('users', 'maryja', updatedMaryjosObj);
     console.log('Ar pavyko atnaujinti Maryja:', updatedMaryja);
 
-    const deletedMaryja = await _data.delete('users', 'maryja');
+    const deletedMaryja = await _data.delete('users', 'maryja');   // trinam faila
     console.log('Ar istryne Maryja:', deletedMaryja);
 
     const users = await _data.list('marks');
